86bc6bf9bad1cf7f1de7afbd8b5e0d2df5e5388d
SCBProd <scbproduktion@gmail.com>
2026-07-12 16:01:00 +0200

    vers

--- app/(private routes)/profile/@my/my/MyStoriesClient.tsx
+++ app/(private routes)/profile/@my/my/MyStoriesClient.tsx
@@ -1,28 +1,29 @@
-'use client'
+'use client';
+
 import { Story } from '@/types/story';
 import css from './MyStoriesClient.module.css';
-import StoriesList from '@/components/ProfilePage/StoriesList/StoriesList'
+import StoriesList from '@/components/ProfilePage/StoriesList/StoriesList';
 import Link from 'next/link';
 
 interface Props {
-Ownstories:Story[]
+  stories: Story[];
 }
-export default function MyStoriesClient({Ownstories}:Props){
-    
-    
-    return (
-  <>
-    {Ownstories && Ownstories.length > 0 ? (<StoriesList stories={Ownstories}/>) : (
-      <>
+
+export default function MyStoriesClient({ stories }: Props) {
+  return (
+    <>
+      {stories && stories.length > 0 ? (
+        <StoriesList stories={stories} />
+      ) : (
         <div className={css.null_stories_div}>
           <h2 className={css.text}>
             Ви ще нічого не публікували, поділіться своєю першою історією!
           </h2>
-          <Link href={'/new-story'} className={css.stories_link}>Опублікувати історію</Link>
+          <Link href="/new-story" className={css.stories_link}>
+            Опублікувати історію
+          </Link>
         </div>
-      </>
-    )}
-  </>
-);
-    
+      )}
+    </>
+  );
 }
