64e956acd60117b16b0f5129c3f6e22dc2080dee
vladislavgradoblyanskyi <vladislavgradoblyanskyi@gmail.com>
2026-07-13 13:33:59 +0200

    Update page.tsx

--- app/(private routes)/profile/@my/my/page.tsx
+++ app/(private routes)/profile/@my/my/page.tsx
@@ -1,11 +1,12 @@
 import { GetOwnStoriesServer } from '@/lib/api/serverApi';
 import MyStoriesClient from './MyStoriesClient';
+import { StoriesResponse } from '@/types/story';
 
 export default async function MyStoriesPage() {
   const res = await GetOwnStoriesServer();
   
 
   return (
-    <MyStoriesClient res={res}/>
+    <MyStoriesClient res={res as unknown as StoriesResponse}/>
   );
 }
